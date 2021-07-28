f53c03dc4f1a75388230d13b661fbb64b36f16b5
Vinicius Alberti <73914923+albertivini@users.noreply.github.com>
2021-07-28 19:02:48 -0300

    fix: show all articles

--- src/useCases/ShowAllArticles/ShowAllArticlesUseCase.ts
+++ src/useCases/ShowAllArticles/ShowAllArticlesUseCase.ts
@@ -1,10 +1,12 @@
 import { getCustomRepository } from "typeorm";
 import { ArticleRepositories } from "../../repositories/ArticleRepositories";
+import { FavoriteRepositories } from "../../repositories/FavoriteRepositories";
 
 export class ShowAllArticlesUseCase {
 
     async execute () {
         const articleRepositories = getCustomRepository(ArticleRepositories)
+        const favoriteRepositories = getCustomRepository(FavoriteRepositories)
 
         const busca = await articleRepositories.find({
             relations: [
@@ -12,7 +14,14 @@ export class ShowAllArticlesUseCase {
             ]
         })
 
-        const dados = busca.map(dado => {
+        const dados = Promise.all(busca.map(async dado => {
+
+            const count = await favoriteRepositories.count({
+                where: {
+                    idPost: dado.id
+                }
+            })
+            
             return {
                 slug: dado.slug,
                 title: dado.title,
@@ -21,14 +30,14 @@ export class ShowAllArticlesUseCase {
                 taglist: JSON.parse(dado.taglist),
                 createdAt: dado.created_at,
                 updatedAt: dado.updated_at,
-                favoritesCount: "ainda vou fazer",
+                favoritesCount: count,
                 author: {
                     username: dado.idUser.username,
                     bio: dado.idUser.bio,
                     image: dado.idUser.image
                 }
             }
-        })
+        }))
 
         return dados
 
